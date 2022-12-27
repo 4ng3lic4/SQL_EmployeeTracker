53da7250fe6fd59a6ebde9a6c868d487b35fa7a3
4ng3lic4 <ositosviajeros@hotmail.com>
2022-12-26 20:10:37 -0500

    added README and more code to schema and index

--- db/schema.sql
+++ db/schema.sql
@@ -7,12 +7,26 @@ CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
 -- Creates the table "table1" within employeetracker_db --
-CREATE TABLE table1(
+CREATE TABLE department(
   -- Creates a numeric column called "id" --
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY, 
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE role(
+   id INT NOT NULL PRIMARY KEY, 
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL NOT NULL,
+   department_id INT NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE employee(
+   id INT NOT NULL PRIMARY KEY, 
+   first_name VARCHAR(30) NOT NULL,
+     last_name VARCHAR(30) NOT NULL,
+     role_id INT NOT NULL PRIMARY KEY, 
+     manager_id INT NOT NULL PRIMARY KEY, 
+);
 
     -- Run in CLI with: source schema.sql --
--- index.js
+++ index.js
@@ -1,2 +1,75 @@
 const inquirer = require("inquirer");
+// Import and require mysql2
+const mysql = require('mysql2');
+const cTable = require("console.table");
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: '12345678',
+        database: 'employeetracker_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
 
+function menu() {
+    inquirer.prompt({
+        type: "input",
+        name: "action",
+        message: "what would you like to do?",
+        choices: [
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View all departments",
+            "View all employes"
+        ]
+    })
+        .then(answers => {
+            if (answers.action === "Update Employee Role") {
+
+            } else if (answers.action === "View All Roles") {
+                viewRole()
+            } else if (answers.action === "Add Role") {
+                viewRole()
+            } else if (answers.action === "View all departments") {
+                viewDepartment();
+
+            } else if (answers.action === "View all employes") {
+                viewEmployee() 
+            } 
+        });
+}
+function viewDepartment() {
+    db.query("SELECT * FROM department", function (error, data) {
+        console.table(data)
+        menu()
+    })
+}
+
+function viewRole() {
+    db.query("SELECT * FROM role", function (error, data) {
+        console.table(data)
+        menu()
+    })
+}
+
+function viewEmployee() {
+    db.query("SELECT * FROM employee", function (error, data) {
+        console.table(data)
+        menu()
+    })
+}
+
+
+//add department
+
+function addDepartment(){
+    inquirer.prompt({
+        type: "input",
+        name: "department",
+        message: "what what is the department you want to add?",
+    })
+}
